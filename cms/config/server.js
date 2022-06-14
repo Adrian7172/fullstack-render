bc97bc5e3f6ba7c6ea9eddf97de4b0aa7db2c432
Adrian7172 <amnchauhan7172@gmail.com>
2022-06-14 15:14:24 +0530

    s

--- cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '70a0b7df8b9bd7868bbe18e1a8d3499e'),
-    },
-  },
-});
